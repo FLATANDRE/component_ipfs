630051ccfc6b63281ff9f255541c22d3e46db25e
Andre Santana <andre.s@lyncas.net>
2023-05-24 07:53:56 -0300

    ajuste nas configuracoes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@ kubectl apply -f namespace/
 kubectl apply -f secrets/
 kubectl apply -f configmap/
 kubectl apply -f services/
-kubectl apply -f deployments/
 kubectl apply -f statefulsets/
-kubectl apply -f ingress/
+#kubectl apply -f ingress/
